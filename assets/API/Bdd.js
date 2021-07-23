c5d016c49a32342f5e7fd55a6a9ff6907565e919
stenny453 <stennygabrielrmr@gmail.com>
2021-07-23 16:45:20 +0200

    New API

--- assets/API/Bdd.js
+++ assets/API/Bdd.js
@@ -3,8 +3,8 @@ export default {
 	// api_url: "https://appsante.herokuapp.com/api/user",
 	// api_url_second: "https://appsante.herokuapp.com/api/seconUser",
 	// socket_url : "https://appsante.herokuapp.com"
-	api_url: "http://adequa.club:3000/api/user",
-	api_url_second: "http://adequa.club:3000/api/seconUser",
+	api_url: "http://ec2-3-130-208-136.us-east-2.compute.amazonaws.com:3001/api/user",
+	api_url_second: "http://ec2-3-130-208-136.us-east-2.compute.amazonaws.com:3001/api/seconUser",
 	pict_url: "https://res.cloudinary.com/hb314jso8/image/upload/v1585234470/",
-	socket_url : "http://adequa.club:3000"
+	socket_url : "http://ec2-3-130-208-136.us-east-2.compute.amazonaws.com:3001"
 }
